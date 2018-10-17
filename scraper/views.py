bdb514c3c2eb53715e32ef3d81b0fd1ba39b7d1e
Dean Hooker <deanhooker91@gmail.com>
2018-10-16 21:59:19 -0600

    homepage

--- scraper/views.py
+++ scraper/views.py
@@ -8,7 +8,11 @@ from .models import Article
 # Create your views here.
 def index(request):
     """The home page for scraper."""
-    return render(request, 'scraper/index.html')
+    articles = Article.objects.all()
+    context = {
+        'article_list': articles,
+    }
+    return render(request, 'scraper/index.html', context)
 
 def scrape(request):
     website = 'https://www.quantamagazine.org' # base url used to create links, can't end with '/'
@@ -24,13 +28,6 @@ def scrape(request):
         titleElem = elem.select('.card__title')
         excerptElem = elem.select('.card__excerpt')
         linkElem = elem.select('.card__content > a')
-
-        # articleDict = {
-        #     'title': titleElem[0].getText(),
-        #     'excerpt': excerptElem[0].getText().strip(),
-        #     'link': website + linkElem[0]['href'],
-        # }
-        # articles.append(articleDict)
 
         new_article = Article()
         new_article.title = titleElem[0].getText()
